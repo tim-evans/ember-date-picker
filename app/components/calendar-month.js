7d7bf56b5f373d18faade5dac034c75bdf620861
Tim Evans <tim.evans@paddle8.com>
2014-11-12 17:59:43 -0500

    remove customizations from moment

--- app/components/calendar-month.js
+++ app/components/calendar-month.js
@@ -1,8 +1,2 @@
 import CalendarMonth from "ember-date-picker/components/calendar-month";
-import moment from "moment";
-
-moment.locale('en', {
-  weekdaysMin : "S_M_T_W_R_F_S".split("_")
-});
-
 export default CalendarMonth;
